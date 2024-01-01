a63b2c1ac2e31663090bbd0ae14268c2579bfa45
Ibraiz <ibraizqazi@gmail.com>
2024-01-01 13:00:35 +0500

    Initial Commit

---
+++ MiniMeetsApp/MiniMeetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMeetsAppApp.swift
+//  MiniMeetsApp
+//
+//  Created by Ibraiz Qazi on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiniMeetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
